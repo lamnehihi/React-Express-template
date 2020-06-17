2fe7d6668d495377fa0ddbdee10db90f53b8b32f
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-06-17 20:06:27 +0700

    update posts model

--- api/models/posts.model.js
+++ api/models/posts.model.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 var postSchema = new mongoose.Schema({
   title: String,
-  content: String
+  content: String,
+  cover: String
 });
 
 var Posts = mongoose.model("Posts", postSchema, "Posts");
